1aa0fb1e3a2e8068ef3d887c6dbe3c37763f6fbf
Tomas Votruba <tomas.vot@gmail.com>
2025-11-21 13:41:14 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! misc

---
+++ ecs.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withPreparedSets(
+        spaces: true,
+        namespaces: true,
+        docblocks: true,
+        arrays: true,
+        comments: true,
+    );
